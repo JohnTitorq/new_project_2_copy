d7021a032663f1751ca2d6635e86d25842f65716
JohnTitor <johntitorqwqqwqqwq@yahoo.com>
2023-08-09 21:34:43 +0300

    dz lesson 5, унаследован шаблон base.html для всех файлов.

--- advertisements/app_advertisements/urls.py
+++ advertisements/app_advertisements/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
-from .views import index
+from .views import index, top_sellers, register, profile, advertisement_post, login, advertisement, advertisement_post
 
 
 urlpatterns=[
-    path("", index)
+    path("", index, name="main-page"),
+    path("top-sellers/", top_sellers, name="top-sellers"),
+    path("register/", register, name="register"),
+    path("login/", login, name="login"),
+    path("profile/", profile, name="profile"),
+    path("advertisement-post/", advertisement_post, name="advertisement-post"),
+    path("advertisement/", advertisement, name="advertisement")
 ]
